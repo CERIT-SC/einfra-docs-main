f0f90fae429c8555a50686c4db2a840b952ca824
Ivana Krenkova <42272325+ivka99@users.noreply.github.com>
2025-04-13 13:19:54 +0200

    Update page.tsx
    deleted repeated link  <li><a href="https://docs.du.cesnet.cz/" className="text-fd-primary flex items-center gap-2"><MoveRight /> Synchronisation&Sharing</a></li>

--- app/(home)/page.tsx
+++ app/(home)/page.tsx
@@ -31,7 +31,6 @@ export default function HomePage() {
           <p>Need to store or share the data? Read what capabilities you have.</p>
           <ul className="pt-2">
            <li><a href="https://docs.du.cesnet.cz/" className="text-fd-primary flex items-center gap-2"><MoveRight /> Overview</a></li>
-           <li><a href="https://docs.du.cesnet.cz/" className="text-fd-primary flex items-center gap-2"><MoveRight /> Synchronisation&Sharing</a></li>
            <li><a href="https://docs.du.cesnet.cz/en/docs/object-storage-s3/s3-service" className="text-fd-primary flex items-center gap-2"><MoveRight /> Object Storage</a></li>
            <li><a href="https://docs.nrp.eosc.cz/" className="text-fd-primary flex items-center gap-2"><MoveRight /> National Data Repositories (NRP)</a></li>
            <li><a href="https://docs.du.cesnet.cz/en/docs/collaboration/owncloud" className="text-fd-primary flex items-center gap-2"><MoveRight /> OwnCloud</a></li>
