9520e92788409c8f32c70de6e19c670915f1d453
Ivana Krenkova <42272325+ivka99@users.noreply.github.com>
2026-03-10 18:43:39 +0100

    Update page.tsx
    link to ondemand

--- app/(home)/page.tsx
+++ app/(home)/page.tsx
@@ -48,7 +48,7 @@ export default function HomePage() {
            <li><a href="https://docs.cerit.io/en/docs/web-apps/foldify" className="text-fd-primary flex items-center gap-2"><MoveRight /> Foldify – Alphafold UI</a></li>
            <li><a href="https://docs.metacentrum.cz/en/docs/software/sw-list/jupyter" className="text-fd-primary flex items-center gap-2"><MoveRight /> Jupyter Notebooks/Hub</a></li>
            <li><a href="https://docs.metacentrum.cz/en/docs/related/galaxy" className="text-fd-primary flex items-center gap-2"><MoveRight /> Galaxy web-based platform</a></li>
-           <li><a href="https://docs.metacentrum.cz/en/docs/ondemand" className="text-fd-primary flex items-center gap-2"><MoveRight /> MetaCentrum&lsquo;s Open OnDemand</a></li>
+           <li><a href="https://docs.metacentrum.cz/en/docs/graphical/ondemand" className="text-fd-primary flex items-center gap-2"><MoveRight /> MetaCentrum&lsquo;s Open OnDemand</a></li>
           </ul>
         </Card>
         <Card title="AI as a Service (AIaaS)" icon={<BrainCircuit />}>
